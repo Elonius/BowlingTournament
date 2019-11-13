d5fd14848227e3caab947462670bff5590aca8af
Shawn <38675302+Elonius@users.noreply.github.com>
2019-11-12 20:42:29 -0400

    GetTeams now works
    Reads database and displays the bowling teams

---
+++ db/ConnectionManager.php
@@ -0,0 +1,11 @@
+<?php
+
+class ConnectionManager {
+
+    public function connect_db() {
+        $db = new PDO("mysql:host=localhost:3310;dbname=BOWLINGTOURNAMENT", "shawnmcc", "shawnmcc");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // throw exceptions
+        return $db;
+    }
+
+}
