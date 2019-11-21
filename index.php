c91fc9eed9464196061a6b938a063f8762d0cf12
Shawn <38675302+Elonius@users.noreply.github.com>
2019-11-21 15:32:28 -0400

    Latest update, plz work

--- index.php
+++ index.php
@@ -7,12 +7,40 @@
         <script src="main.js"></script>
     </head>
     <body>
-        <h1>Bowling Tournament - Guest</h1>
-
-        <button id="getTeams">Get Teams</button>
-        <button id="getPlayers">Get Players</button>
-        <br>
-        <!--<button id="AddButton" class="hidden">Add Weapon</button>-->
+        <div id="container">
+            <div class="flex-container">
+                <div id="public" class="flex-item">
+                    <h1>Guest/Public</h1>
+                    <button id="getTeams">View Teams</button><br>
+                    <button id="getStandings" disabled>View Standings</button><br>
+                </div>
+                <div id="admin" class="flex-item">
+                    <h1>Admin</h1>
+                    <button id="adminGetTeams">View Teams</button><br>
+                    <button id="adminGetPlayers">View Players</button><br>
+                </div>
+            </div>
+            <div id="tableDiv" class="flex-container">
+                <table id="teamTable" class="hidden">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Earnings</th>
+                        <th></th>
+                    </tr>
+                </table>
+                <table id="playerTable" class="hidden">
+                    <tr>
+                        <th>ID</th>
+                        <th>Team ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Hometown</th>
+                        <th>Province</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
 
         <!--        <div id="AddUpdatePanel">
                     <div id="weapID" class="hidden">
@@ -39,13 +67,7 @@
                     </div>
                 </div>-->
 
-        <table id="teamTable" class="hidden">
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Earnings</th>
-                <th></th> <!-- For 'View Players' button -->
-            </tr>
-        </table>
+
+
     </body>
 </html>
--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 window.onload = function () {
-
+    window.user = "";
     // add event handlers for buttons
-    document.querySelector("#getTeams").addEventListener("click", getTeams);
-    document.querySelector("#getPlayers").addEventListener("click", getPlayers);
+    document.querySelector("#getTeams").addEventListener("click", guestGetTeams);
+    document.querySelector("#adminGetTeams").addEventListener("click", adminGetTeams);
+    document.querySelector("#adminGetPlayers").addEventListener("click", adminGetAllPlayers);
+
+//    document.querySelector("#getPlayers").addEventListener("click", getPlayers);
 
 //    hideUpdatePanel();
 };
+
+function guestGetTeams() {
+    user = "guest";
+    getTeams();
+}
+
+function adminGetTeams() {
+//    debugger;
+    user = "admin";
+    getTeams();
+}
+
+function adminGetAllPlayers() {
+    user = "admin";
+    getAllPlayers();
+}
 
 // make AJAX call to PHP to get JSON data
 function getTeams() {
@@ -17,7 +36,7 @@ function getTeams() {
             if (resp.search("ERROR") >= 0) {
                 alert("oh no...Check console");
             } else {
-                buildTable(xmlhttp.responseText);
+                buildTeamTable(resp);
             }
         }
     };
@@ -27,7 +46,29 @@ function getTeams() {
 //    document.querySelector("#GetButton").classList.add("hidden");
 }
 
-function getPlayers() {
+// Used for the buildTeams function
+function getPlayers(e) {
+    let id = (e.target.parentElement.parentElement.cells[0].innerHTML);
+    let url = "BowlingTournament/players/" + id;
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            let resp = xmlhttp.responseText;
+            if (resp.search("ERROR") >= 0) {
+                alert("oh no...Check console");
+            } else {
+                buildPlayerTable(resp);
+            }
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+//    document.querySelector("#GetButton").classList.add("hidden");
+}
+
+function getAllPlayers() {
+//    let id = (e.target.parentElement.parentElement.cells[0].innerHTML);
     let url = "BowlingTournament/players";
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -36,8 +77,7 @@ function getPlayers() {
             if (resp.search("ERROR") >= 0) {
                 alert("oh no...Check console");
             } else {
-                console.log(xmlhttp.responseText);
-                buildTable(xmlhttp.responseText);
+                buildPlayerTable(resp);
             }
         }
     };
@@ -48,24 +88,36 @@ function getPlayers() {
 }
 
 // text is a JSON string containing an array
-function buildTable(text) {
+function buildTeamTable(text) {
     let arr = JSON.parse(text);
-    let theTable = document.querySelector("table");
+    let theTable = document.querySelector("#teamTable");
     let html = theTable.querySelector("tr").innerHTML;
 
 //    document.querySelector("#AddButton").classList.remove("hidden");
     document.querySelector("#teamTable").classList.remove("hidden");
+    document.querySelector("#playerTable").classList.add("hidden");
+    html += "<div>";
+    if (user === "admin") {
+        html += "<button>Add a Team</button><br>";
+    }
 
+    html += "<h3>All teams in tournament</h3>";
+    html += "<table>";
     for (let i = 0; i < arr.length; i++) {
         let row = arr[i];
         html += "<tr>";
         html += "<td>" + row.teamID + "</td>";
         html += "<td>" + row.teamName + "</td>";
-        html += "<td>" + row.earnings + "</td>";
+        if (row.earnings === null) {
+            html += "<td>$0.00</td>";
+        } else {
+            html += "<td>" + row.earnings + "</td>";
+        }
         html += "<td><button class=viewPlayers>View Players</button></td>";
         html += "</tr>";
     }
-
+    html += "</table></div>";
+    console.log(html);
     theTable.innerHTML = html;
 
     let players = document.querySelectorAll(".viewPlayers");
@@ -75,4 +127,32 @@ function buildTable(text) {
         let temp = players[i];
         temp.addEventListener("click", getPlayers);
     }
+}
+
+// text is a JSON string containing an array
+function buildPlayerTable(text) {
+    let arr = JSON.parse(text);
+    let theTable = document.querySelector("#playerTable");
+    let html = theTable.querySelector("tr").innerHTML;
+    html += "<div>";
+    if (user === "admin") {
+        html += "<button type=submit>Add a Player</button>";
+    }
+//    document.querySelector("#AddButton").classList.remove("hidden");
+    document.querySelector("#playerTable").classList.remove("hidden");
+    document.querySelector("#teamTable").classList.add("hidden");
+    html += "<table>";
+    for (let i = 0; i < arr.length; i++) {
+        let row = arr[i];
+        html += "<tr>";
+        html += "<td>" + row.playerID + "</td>";
+        html += "<td>" + row.teamID + "</td>";
+        html += "<td>" + row.firstName + "</td>";
+        html += "<td>" + row.lastName + "</td>";
+        html += "<td>" + row.hometown + "</td>";
+        html += "<td>" + row.province + "</td>";
+        html += "</tr>";
+    }
+    html += "</table>";
+    theTable.innerHTML = html;
 }
